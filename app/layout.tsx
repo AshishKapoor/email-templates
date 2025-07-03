0e69324e4169bdf59859a1b3c4a6c819b77a1eda
AshishKapoor <AshishKapoor@users.noreply.github.com>
2025-07-03 13:37:13 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Email Generator Pro',
+  description: 'Created by Sannty',
+  generator: 'sannty.in',
 }
 
 export default function RootLayout({
